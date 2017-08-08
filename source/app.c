c8dbbf2cd922a35c70faa616c699a792ad697448
autocommit <autocommit>
2017-08-08 20:31:44 +0000

    auto 0 6 85

--- source/app.c
+++ source/app.c
@@ -25,6 +25,9 @@ int main(int argc, char**argv) {
 															 HTML_PARSE_RECOVER |
 															 HTML_PARSE_NONET |
 															 HTML_PARSE_COMPACT);
+	// html5 please
+	doc->intSubset->ExternalID = NULL;
+		doc->intSubset->SystemID = NULL;
 	htmlNodeDumpFileFormat(stderr,doc,doc,"UTF-8",1);
 	ensure_ne(NULL,doc)
 	html_when((xmlNode*)doc); // magic...
