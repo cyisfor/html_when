19b7860a418645eb07c3e1c591d23e7743d8aa33
autocommit <autocommit>
2017-08-13 19:44:56 +0000

    auto 0 38 526

--- src/cleanup.h
+++ src/cleanup.h
@@ -1,5 +1,11 @@
-#define DECLARE_CLEANUP(name,type) static inline void cleanup_ ## name(type* v) { name(*v); }
-DECLARE_CLEANUP(fclose,FILE*);
-DECLARE_CLEANUP(close,in);
-DECLARE_CLEANUP(free,void*);
+#define DECLARE_CLEANUP(name,type) static inline void cleanup_ ## name(type* v) { name(*v); *v = 0; }
+//#include <stdio.h>
+//DECLARE_CLEANUP(fclose,FILE*);
+//#include <unistd.h>
+//DECLARE_CLEANUP(close,in);
+//#include <stdlib.h>
+//DECLARE_CLEANUP(free,void*);
+//#include <dirent.h>
+//DECLARE_CLEANUP(closedir,DIR*);
 #define cleanup(a) __attribute__((__cleanup__(cleanup_ ## a)))
+//cleanup(closedir) DIR* d = opendir(...);
