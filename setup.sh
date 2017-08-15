60e8a1cde31ad6684871b08d835ac149f047e1e2
autocommit <autocommit>
2017-08-15 00:17:51 +0000

    auto 0 71 773

--- setup.sh
+++ setup.sh
@@ -1,33 +1,16 @@
-function sync {
-		if [[ -d $dir ]]; then
-				source=$dir
-				adjremote=1
-		else
-				source=$remote
-		fi
-		if [[ -d $dest ]]; then
-				[[ -n "$nocheck" ]] && return
-				cd $dest
-				git pull local master
-				git pull origin master
-				cd ..
-		else
-				git clone $source $dest
-				if [[ -n "$adjremote" ]]; then
-						cd $dest
-						git remote set-url origin $remote
-						git remote add local $source
-						cd ..
-				fi
-		fi
-}
-	
-dir=/extra/home/packages/git/libxml2
+. git-tools/funcs.sh
+
+local=/home/code/git-tools
+remote=https://github.com/cyisfor/git-tools.git
+dest=git-tools
+sync
+
+local=/extra/home/packages/git/libxml2
 remote=git://git.gnome.org/libxml2
 dest=libxml2
 sync
 
-dir=/home/code/note
+local=/home/code/note
 remote=https://github.com/cyisfor/note.git
 dest=note
 sync
