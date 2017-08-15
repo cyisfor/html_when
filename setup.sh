081b7601614504744db482aa172da7b008c27319
autocommit <autocommit>
2017-08-15 00:24:01 +0000

    auto 0 6 39

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 local=/home/code/git-tools
 remote=https://github.com/cyisfor/git-tools.git
 dest=git-tools
-sync
+clonepull
 
 local=/extra/home/packages/git/libxml2
 remote=git://git.gnome.org/libxml2
 dest=libxml2
-sync
+clonepull
 
 local=/home/code/note
 remote=https://github.com/cyisfor/note.git
 dest=note
-sync
+clonepull
